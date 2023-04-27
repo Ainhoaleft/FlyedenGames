e3d2276c364eeb320208bd9185ecf5577dd7a740
Ainhoaleft <izquierdo19668@iesmarenostrum.com>
2023-04-27 20:34:49 +0200

    Modificando la parte front de admin

---
+++ MvcTienda_prueba/MvcTienda/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcTienda.Data;
+using MvcTienda.Models;
+using System.Diagnostics;
+
+namespace MvcTienda.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly MvcTiendaContexto _context;
+
+        public HomeController(ILogger<HomeController> logger, MvcTiendaContexto context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            // Busca el empleado correspondiente al usuario actual. Si existe, activa la
+            // vista (View) y en caso contrario, se redirige para crear el empleado.
+            string? emailUsuario = User.Identity.Name;
+            Cliente? empleado = _context.Clientes.Where(e => e.Email == emailUsuario)
+            .FirstOrDefault();
+            if (User.Identity.IsAuthenticated &&
+            User.IsInRole("Usuario") &&
+            empleado == null)
+            {
+                return RedirectToAction("Create", "MisDatos");
+            }
+            return View();
+            //return RedirectToAction("Index", "Escaparate");    
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
